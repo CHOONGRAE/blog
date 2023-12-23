bc71a7e1223e8bbd220ae151af030c58159972f9
CHOONGRAE <choongraej@gmail.com>
2023-12-23 17:55:18 +0900

    Feat: BooleanContainer

---
+++ src/components/common/Container/BooleanContainer/index.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { Children, cloneElement, useState } from 'react'
+import * as S from '@/styles/common/Container/BooleanContainer'
+
+interface BooleanContainerProps {
+  children: React.ReactElement
+  init?: boolean
+  propName?: string
+}
+
+const BooleanContainer = ({
+  children,
+  init = false,
+  propName = 'boolean',
+  ...args
+}: BooleanContainerProps) => {
+  const [boolean, setBoolean] = useState(init)
+
+  const clickHandler = () => setBoolean((prev) => !prev)
+
+  const onlyChild = Children.only(children)
+
+  const childWithProps = cloneElement(onlyChild, { [propName]: boolean })
+
+  return (
+    <S.BooleanConainer onClick={clickHandler} {...args}>
+      {childWithProps}
+    </S.BooleanConainer>
+  )
+}
+
+export default BooleanContainer
